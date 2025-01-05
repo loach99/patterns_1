81f31b8288c7ee7b9e1ec46ff732310c200fa2a6
Константин Вьюнков <113593006+loach99@users.noreply.github.com>
2025-01-05 21:14:48 +0300

    Update TestData.java

--- src/test/java/TestData.java
+++ src/test/java/TestData.java
@@ -6,7 +6,7 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestData {
     @BeforeEach
